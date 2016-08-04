254d04f4ab16bc862ef7323206cb69abe3c76f68
Jon Lauridsen <mail@jonlauridsen.com>
2016-08-05 00:51:13 +0100

    All files should be strict

--- test/lib.morseify.spec.js
+++ test/lib.morseify.spec.js
@@ -1,3 +1,4 @@
+"use strict";
 const expect = require("chai").expect
 const _ = require("lodash")
 const morseify = require("../lib/morseify")
--- test/lib.obfuscator.spec.js
+++ test/lib.obfuscator.spec.js
@@ -1,3 +1,4 @@
+"use strict";
 const expect = require("chai").expect
 const obfuscator = require("../lib/obfuscator")
 
