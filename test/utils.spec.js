a81a4db44c4892d4cd71f0ab7c9d6b8cc6cbad7f
Jon Lauridsen <mail@jonlauridsen.com>
2016-08-05 10:10:19 +0100

    Quote consistency

--- test/utils.spec.js
+++ test/utils.spec.js
@@ -1,11 +1,11 @@
 "use strict";
 const expect = require("chai").expect
-const utils = require('../lib/utils')
+const utils = require("../lib/utils")
 
-describe('utils', () => {
-  describe('#last', ()=> {
-    it('returns last element in array', () => {
-      expect(utils.last(['a', 'b'])).to.eql('b')
+describe("utils", () => {
+  describe("#last", ()=> {
+    it("returns last element in array", () => {
+      expect(utils.last(["a", "b"])).to.eql("b")
     })
   })
 })
