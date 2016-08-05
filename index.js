1867504ca0f26d7b4b6e168d4b9103099251e6ae
Jon Lauridsen <mail@jonlauridsen.com>
2016-08-05 10:29:12 +0100

    Use punctuator lib

--- index.js
+++ index.js
@@ -2,21 +2,9 @@
 const fs = require("fs")
 const morseify = require("./lib/morseify")
 const obfuscator = require("./lib/obfuscator")
-const utils = require("./lib/utils")
+const punctuator = require("./lib/punctuator")
 
 const DEFAULT_CONVERTTEXT_OPTIONS = {obfuscate: false, separators: {char: "|", word: "/"}}
-
-const addPunctuation = (value, prevVal, wordSeparator, charSeparator) => {
-  charSeparator = charSeparator || ""
-  wordSeparator = wordSeparator || " "
-  if (value === " ") {
-    return [wordSeparator]
-  } else if (value === "\n" || prevVal === "\n" || prevVal === wordSeparator) {
-    return [value]
-  } else {
-    return [charSeparator, value]
-  }
-}
 
 exports.processArgv = (argv) => {
   const msg = argv._.join(" ") || fs.readFileSync(argv.file, "utf8")
@@ -33,13 +21,6 @@ exports.convertText = (message, opts) => {
   opts = Object.assign({}, DEFAULT_CONVERTTEXT_OPTIONS, opts)
   const morsed = morseify(message)
   const output = opts.obfuscate ? obfuscator(morsed) : morsed
-
-  return output.reduce((result, value) => {
-    if (result.length === 0) {
-      result.push(value)
-    } else {
-      result.push(...addPunctuation(value, utils.last(result), opts.separators.word, opts.separators.char))
-    }
-    return result
-  }, []).join("")
+  const punctuated = punctuator(output, opts.separators)
+  return punctuated.join("")
 }
